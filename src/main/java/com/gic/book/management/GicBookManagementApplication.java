e7880be88e996321564e968dd869ce227039a530
Siyabonga Mdletshe <siyabonga.w.mdletshe@gmail.com>
2025-06-18 11:44:13 +0200

    feat: adds initial source files generated using spring initializr
    https://start.spring.io/

---
+++ src/main/java/com/gic/book/management/GicBookManagementApplication.java
@@ -0,0 +1,13 @@
+package com.gic.book.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GicBookManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GicBookManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gic/book/management/GicBookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gic.book.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GicBookManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
